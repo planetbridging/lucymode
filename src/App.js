32e7f19753fee51e142705ca724da11008a4a833
shannon <plantbridging@gmail.com>
2021-09-24 10:10:15 +1000

    bedrock

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TG from './templateGenerator/tg';
+function App() {
+  return (
+    <div className="App">
+      <TG />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/templateGenerator/tg.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class TG extends React.Component {
+
+    state = {
+    }
+
+    componentDidMount(){
+        
+    }
+
+    render(){
+        return (
+            <div>
+                helo
+            </div>
+        )
+    }
+}
+
+export default TG;
